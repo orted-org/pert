faefa4affec380840d0c52dfed72da4f39b509e0
himanshu-sah <itsmehs07@gmail.com>
2021-11-04 10:53:04 +0545

    unit test for kv store

--- src/DB/DB.KV.ts
+++ src/DB/DB.KV.ts
@@ -7,7 +7,7 @@ import {
 } from "../Helpers/Wrapper/Redis";
 
 interface IKVStore {
-  Set: (key: string, value: string, expiryTime: number) => Promise<void>;
+  Set: (key: string, value: string, expiryTime?: number) => Promise<void>;
   Get: (key: string) => Promise<string | null>;
   Delete: (key: string) => Promise<void>;
   Truncate: () => Promise<void>;
